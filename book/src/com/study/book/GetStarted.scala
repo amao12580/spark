ca79aa65ecbe8d5239c9c3b100476fb657c79399
成亮 <chengl@zhengtongauto.net>
2019-04-08 17:43:49 +0800

    学习Scala-Spark最佳实践.pdf-01安装scala开发环境

--- book/src/com/study/book/GetStarted.scala
+++ book/src/com/study/book/GetStarted.scala
@@ -5,6 +5,8 @@ package com.study.book
   * User:ChengLiang
   * Date:2019/4/8
   * Time:17:37
+  *
+  * https://blog.csdn.net/Fly964747264/article/details/78950193
   */
 object GetStarted {
   def main(args: Array[String]): Unit = {
