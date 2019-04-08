9ae871e0eb9d2d60848f0b9f268a61c820e69192
成亮 <chengl@zhengtongauto.net>
2019-04-08 17:41:03 +0800

    学习Scala-Spark最佳实践.pdf-01安装scala开发环境

---
+++ book/src/com/study/book/GetStarted.scala
@@ -0,0 +1,13 @@
+package com.study.book
+
+/**
+  * Created with IntelliJ IDEA.
+  * User:ChengLiang
+  * Date:2019/4/8
+  * Time:17:37
+  */
+object GetStarted {
+  def main(args: Array[String]): Unit = {
+    println("hello world!", args.toString)
+  }
+}
